837f31f09fe8a078d956e580cd6aecabb0c7f238
Isis Gonçalves Santos <isissantos8425@gmail.com>
2024-03-21 16:14:35 -0300

    criando função dropdown do catálogo

--- src/components/head/index.tsx
+++ src/components/head/index.tsx
@@ -1,8 +1,16 @@
-import styled from "styled-components";
+import { Container, Header, ContentBanner, DropdownList, AnimatedList } from './style'
 import { BsCart3, BsHeart, BsSearch } from "react-icons/bs";
 import headphoneDestaque from '../../images/headphone-1.png'
+import { useState } from 'react';
 
 export const Head = () => {
+    const [open, setOpen] = useState<boolean>(false)
+
+    const handleDropdown = (state: boolean) => {
+        setOpen(!state)
+    }
+    console.log(open);
+
     return (
         <>
             <Container>
@@ -11,10 +19,21 @@ export const Head = () => {
 
                     <nav>
                         <ul>
-                            <li>Catálogo</li>
-                            <div><input type="text" placeholder="Buscar" /> <span><BsSearch /></span></div>
-                            <li><BsHeart /> Curtidos</li>
-                            <li><BsCart3 /> Carrinho</li>
+                            <DropdownList>
+                                <AnimatedList onClick={() => handleDropdown(open)}>Catálogo</AnimatedList>
+                                {open && (
+                                    <ul>
+                                        <li>Celulares</li>
+                                        <li>Headphones</li>
+                                        <li>Smartwatch</li>
+                                    </ul>
+                                )}
+                            </DropdownList>
+                            <div>
+                                <input type="text" placeholder="Buscar" /> <span><BsSearch /></span>
+                            </div>
+                            <AnimatedList><BsHeart /> Curtidos</AnimatedList>
+                            <AnimatedList><BsCart3 /> Carrinho</AnimatedList>
                         </ul>
                     </nav>
                 </Header>
@@ -30,101 +49,3 @@ export const Head = () => {
         </>
     )
 };
-
-const Container = styled.section`
-    background: rgb(101,94,97);
-    background: linear-gradient(270deg, rgba(101,94,97,1) 0%, rgba(37,36,36,1) 100%);
-    min-height: 100vh;
-    color: #fff;
-`;
-
-const Header = styled.header`
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    padding: 15px;
-
-    h1 {
-        background-color: #ffffff;
-        color: #252424;
-        padding: 5px;
-    }
-
-    nav ul {
-        display: flex;
-        gap: 30px;
-        cursor: pointer;
-    }
-
-    div {
-        position: relative;
-        
-
-        input {
-            border: 1px solid #fff;
-            background: transparent;
-            border-radius: 5px;
-            padding: 5px 15px;
-            color: #ffffff;
-            
-            position: relative;
-            &::placeholder {
-                color: #ffffff;
-                padding: 0 10px;
-            }
-        }
-
-        span {
-            position: absolute;
-            color: black;
-            right: 1px;
-            color: #fff;
-            padding: 0 5px;
-            height: 100%;
-            border-radius: 5px;
-            top: 5px;
-        }
-    }
-
-
-    li {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: relative;
-        gap: 8px;
-
-        &::after {
-            content: '';
-            position: absolute;
-            left: 0;
-            bottom: -2px;
-            width: 100%;
-            height: 2px;
-            background-color: #fff;
-            transform: scaleX(0);
-            transform-origin: bottom right;
-            transition: transform 0.3s ease-out;
-        }
-        &:hover::after {
-            transform: scaleX(1);
-            transform-origin: bottom left;
-        }
-    }
-`;
-
-const ContentBanner = styled.section`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 30px;
-    gap: 60px;
-
-    div {
-        max-width: 500px;
-    }
-    
-    img {
-        max-width: 350px;
-    }
-`
---
+++ src/components/head/style.tsx
@@ -0,0 +1,149 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+    background: rgb(101,94,97);
+    background: linear-gradient(270deg, rgba(101,94,97,1) 0%, rgba(37,36,36,1) 100%);
+    min-height: 100vh;
+    color: #fff;
+`;
+
+export const Header = styled.header`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 15px;
+
+    h1 {
+        background-color: #ffffff;
+        color: #252424;
+        padding: 5px;
+    }
+
+    nav ul {
+        display: flex;
+        gap: 30px;
+        cursor: pointer;
+    }
+
+    div {
+        position: relative;
+        
+        input {
+            border: 1px solid #fff;
+            background: transparent;
+            border-radius: 5px;
+            padding: 5px 15px;
+            color: #ffffff;
+            
+            position: relative;
+            &::placeholder {
+                color: #ffffff;
+                padding: 0 10px;
+            }
+        }
+
+        span {
+            position: absolute;
+            color: black;
+            right: 1px;
+            color: #fff;
+            padding: 0 5px;
+            height: 100%;
+            border-radius: 5px;
+            top: 5px;
+        }
+    }
+
+
+    li {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        /* position: relative; */
+        gap: 8px;
+
+        /* &::after {
+            content: '';
+            position: absolute;
+            left: 0;
+            bottom: -2px;
+            width: 100%;
+            height: 2px;
+            background-color: #fff;
+            transform: scaleX(0);
+            transform-origin: bottom right;
+            transition: transform 0.3s ease-out;
+        }
+        &:hover::after {
+            transform: scaleX(1);
+            transform-origin: bottom left;
+        } */
+    }
+`;
+
+export const AnimatedList = styled.li`
+    position: relative;
+    &::after {
+            content: '';
+            position: absolute;
+            left: 0;
+            bottom: -2px;
+            width: 100%;
+            height: 2px;
+            background-color: #fff;
+            transform: scaleX(0);
+            transform-origin: bottom right;
+            transition: transform 0.3s ease-out;
+        }
+        &:hover::after {
+            transform: scaleX(1);
+            transform-origin: bottom left;
+        }
+`
+
+export const DropdownList = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: relative;
+
+    ul {
+        display: flex;
+        flex-direction: column;
+        position: absolute;
+        top: 40px;
+        right: 0;
+        left: -25px;
+        background-color: rgba(255, 255, 255, 0.1);
+        border-radius: 10px;
+        padding: 15px;
+        width: 100px;
+        z-index: 1;
+
+        li {
+        transition: .3s ease-in;
+        padding: 5px;
+
+        &:hover {
+            background-color: #eee;
+            color: black;
+        }
+        }
+    }    
+`;
+
+export const ContentBanner = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* padding: 30px; */
+    height: 80vh;
+    gap: 60px;
+
+    div {
+        max-width: 500px;
+    }
+    
+    img {
+        max-width: 350px;
+    }
+`
